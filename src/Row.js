005260838fab00fe82854668f9f3c25ca5eb1338
NizaV <60785417+NizaV@users.noreply.github.com>
2020-11-09 21:01:52 -0500

    finished rows

--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios'
+import './Row.css'
 
 const base_url = "https://image.tmdb.org/t/p/original";
 
@@ -17,7 +18,7 @@ function Row({ title, fetchUrl }) {
         // };
         async function fetchData() {
             const request = await axios.get(fetchUrl);
-            console.log(request.data.results);
+            // console.log(request.data.results);
             setMovies(request.data.results);
             return request;
         };
@@ -34,7 +35,10 @@ function Row({ title, fetchUrl }) {
             <div className="row_posters">
                 {movies.map(movie => {
                    return (
-                    <img src={`${base_url}${movie.poster_path}`} alt={movie.title}/>
+                    <img
+                    key={movie.id}
+                    className="row_poster"
+                     src={`${base_url}${movie.poster_path}`} alt={movie.title}/>
                    )
                 })}
             </div>
