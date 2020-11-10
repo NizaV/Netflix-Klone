66b0e477c8994ce4a814776c24d14dd43f29cb9b
NizaV <60785417+NizaV@users.noreply.github.com>
2020-11-09 20:27:11 -0500

    linking images

--- src/Row.js
+++ src/Row.js
@@ -1,39 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios'
 
-function Row( {title, fetchUrl} ) {
+const base_url = "https://image.tmdb.org/t/p/original";
+
+function Row({ title, fetchUrl }) {
     const [movies, setMovies] = useState([]);
-    const base_url = "hhtps://image.tmdb.org/t/p/original";
+    
 
     useEffect(() => {
         // if [] are empty, run once when the row loads, and dont run again, basically only run once on load
-        function fetchData() {
-            axios.get(fetchUrl, { crossdomain: "true"})
-            .then(response => setMovies(response.data.results));
+        // function fetchData() {
+        //     axios.get(fetchUrl, { crossdomain: "true"})
+        //     .then(response => setMovies(response.data.results));
+
         
-        };
-        // async function fetchData() {
-        //     var config = {
-        //         headers: {'Access-Control-Allow-Origin': '*'}
-        //     };
-        //     const request = await axios.get(fetchUrl, config);
-        //     console.log(request.data.results);
-        //     setMovies(request.data.results);
-        //     return request;
         // };
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            console.log(request.data.results);
+            setMovies(request.data.results);
+            return request;
+        };
         fetchData();
     }, [fetchUrl]); // <-- the dependency would go inside the bracKets
-    console.log("fetchUrl", fetchUrl);
-    console.log("title", title);
-    console.log("movies", movies);
-
+    // console.log("fetchUrl", fetchUrl);
+    // console.log("title", title);
+    // console.log("movies", movies);
+    console.table(movies);
     return (
         <div className="row">
             <h2> {title} </h2>
 
             <div className="row_posters">
-                {movies.map( movie => {
-                    <img src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
+                {movies.map(movie => {
+                   return (
+                    <img src={`${base_url}${movie.poster_path}`} alt={movie.title}/>
+                   )
                 })}
             </div>
             {/* containers and posters */}
