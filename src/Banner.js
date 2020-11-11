1d4dc739296f4a7d3a700b9974c4dec7fc787510
NizaV <60785417+NizaV@users.noreply.github.com>
2020-11-11 11:00:51 -0500

    finished banner

--- src/Banner.js
+++ src/Banner.js
@@ -22,6 +22,11 @@ function Banner() {
 
     console.log(movie);
 
+    // this function allows the description to be limited to a certain amount of text
+    function truncate(str, n) {
+        return str?.length > n ? str.substr(0, n-1) + "..." : str;
+    };
+
     return (
         <header className="banner"
         style={{
@@ -34,15 +39,21 @@ function Banner() {
         >
             <div className="banner_contents">
                 <h1 className="banner_title">
-                {/* this line is below is cleaner than an entire if then statement */}
+                {/* this line is below is cleaner than an entire if then statement. Optional chaining "?." in full effect */}
                     {movie?.title || movie?.name || movie?.original_name}
                 </h1>
                 <div className="banner_buttons">
                     <button className="banner_button">{"Play"}</button>
-    <button className="banner_button">{"My List"}</button>
-    <h1 className="banner_description">{movie?.overview}</h1>
+                    <button className="banner_button">{"My List"}</button>
                 </div>
+
+                <h1 className="banner_description">
+                    {truncate(movie?.overview, 150)}
+                </h1>
             </div>
+
+            <div className="banner--fadeBottom"></div>
+            
         </header>
     )
 }
