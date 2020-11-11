f034c2d7a44ff63f4b10484c373a4d1c0236b2a6
NizaV <60785417+NizaV@users.noreply.github.com>
2020-11-11 14:48:49 -0500

    finished banner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Row from './Row.js';
+import Nav from './Nav.js';
 import requests from './requests.js';
 import Banner from './Banner.js';
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Banner />
       <Row 
       title="NETFLIX ORIGINALS" 
